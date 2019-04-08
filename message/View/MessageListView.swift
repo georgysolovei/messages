f89a134f3bebda1d6376159e44ef08ca6d8fd710
Georgy Solovei <g.solovey@codeinspiration.pro>
2019-04-08 22:13:19 +0300

    sets new project

---
+++ message/View/MessageListView.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  message
+//
+//  Created by Georgy Solovei on 4/8/19.
+//  Copyright © 2019 home. All rights reserved.
+//
+
+import UIKit
+
+class MessageListView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
